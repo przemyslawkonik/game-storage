cef2cd29b9b980e0a8bee6273a55232921a1fa96
przemyslaw <przemyslawkonik@gmail.com>
2018-02-02 10:29:13 +0100

    menu bar

---
+++ menu_home.php
@@ -0,0 +1,20 @@
+﻿<?php
+	$username;
+	if(isset($_SESSION['username'])) {
+		$username = $_SESSION['username'];
+	}
+	else {
+		$username = "";
+	}
+?>
+
+<nav class="navbar navbar-inverse">
+	<ul class="nav navbar-nav">
+		<li><a href="home.php">Homepage</a></li>
+		<li><a href="home.php">Games</a></li>
+	</ul>
+	<ul class="nav navbar-nav navbar-right">
+		<li><a>Logged as <?php echo $username?></a></li>
+		<li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+	</ul>
+</nav>
---
+++ menu_index.php
@@ -0,0 +1,9 @@
+﻿<nav class="navbar navbar-inverse">
+	<ul class="nav navbar-nav">
+		<li><a href="index.php">Homepage</a></li>
+	</ul>
+	<ul class="nav navbar-nav navbar-right">
+		<li><a href="index.php"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+		<li><a href="register.php"><span class="glyphicon glyphicon-log-in"></span> Register</a></li>
+	</ul>
+</nav>
